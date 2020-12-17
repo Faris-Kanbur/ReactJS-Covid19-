a9fa27d514298219a2ecd0988530576f0c292806
faris <faris38kanbur@gmail.com>
2020-12-17 13:06:40 +0100

    add covid19logo

--- covid/src/App.js
+++ covid/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Grid from "@material-ui/core/Grid";
+
+import covidLogo from "./covid-19.svg"
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Grid container>
+          <img src={covidLogo} alt="Covid19 Logo" style={{height:150, width: 150 , marginTop: 25 }}/>
+
+        </Grid>
+       
+      </Container>
+    </React.Fragment>
   )
 }
 
